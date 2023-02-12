5eb92f2e07b934e09b17d1932e019634fd528825
Andres Garcia <andresgarcia@macbook-pro.mynetworksettings.com>
2023-02-12 11:39:13 -0500

    Edited - multiple page app.

--- main.py
+++ main.py
@@ -3,48 +3,17 @@
 import streamlit as st
 from deta import Deta
 
+st.sidebar.markdown('# Main')
+
 st.write("""
 # Whitewater Log 🌊
 """)
 
-with st.form('log_form'):
-    date = st.date_input('Date')
+# Connect to Deta Base with your Project Key
+deta = Deta(st.secrets['deta_key'])
 
-    river = st.selectbox(
-        'Select a river section',
-        ('Little Falls', 'Great Falls', 'Upper Yough')
-    )
-
-    level = st.number_input('Level', 
-                        min_value=0.0, max_value=20000.0, step=0.01, value=3.0)
-
-    level_type = st.radio(
-                    'FT or CSF',
-                    options=('FT', 'CFS')
-                )
-
-    notes = st.text_input('Notes', max_chars=200)
-
-    # Every form must have a submit button.
-    submitted = st.form_submit_button("Submit")
-
-    # Connect to Deta Base with your Project Key
-    deta = Deta(st.secrets['deta_key'])
-
-    # Create a new database "wwlog-db"
-    db = deta.Base("wwlog-db")
-
-    # If the user clicked the submit button,
-    # write the data from the form to the database.
-    if submitted:
-        st.markdown(f"""
-            📝 **Entry**
-            - Date: `{date}`
-            - River: `{river}`
-            - Level: `{level} {level_type}`
-        """)
-        db.put({'Date':date.strftime('%Y-%m-%d'), 'River': river,
-                'Level':level, 'level_type':level_type, 'Notes':notes})
+# Create a new database "wwlog-db"
+db = deta.Base("wwlog-db")
 
 "---"
 "Here's everything stored in the database:"
