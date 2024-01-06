ddefbdb4eba5ce0b7bf1813407e2b4392781c72f
Andres Garcia <garcia.andres@proton.me>
2024-01-06 16:33:39 -0500

    Added code to deal with the new years

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import psycopg2
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 from sqlalchemy import create_engine, text
 
 def add_record_ww(record):
@@ -12,14 +13,28 @@ def add_record_ww(record):
     engine = create_engine(db_url)
     df.to_sql(table, engine, if_exists='append', index=False)
 
-def get_total_sessions_ww():
+@st.cache_data
+def load_data():
     db_url = st.secrets['db_url']
     table =  st.secrets['ww_table']
     conn = psycopg2.connect(db_url)
     engine = create_engine(db_url)
-    query = f"SELECT COUNT(*) FROM {table}"
-    count_df = pd.read_sql_query(text(query), engine.connect())
-    return count_df.iloc[0][0]
+    query = f"SELECT * FROM {table}"
+    df = pd.read_sql_query(text(query), engine.connect())
+    df['Date'] = pd.to_datetime(df['Date'])
+    return df
+
+def get_total_sessions_ww_curr_year(df, year=datetime.now().year):
+    # extract the year from the date column
+    df['year'] = df['Date'].dt.year
+    # group by year an get the count
+    year_counts = df.groupby('year').size().reset_index(name='count')
+    # return the value for the latest year
+    return year_counts[year_counts['year'] == year]['count'].iloc[0]
+
+
+    print(year_counts)
+    return 1
 
 def get_last_on_river_ww():
     db_url = st.secrets['db_url']
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # main.py
 import streamlit as st
 import pandas as pd
-from db import get_total_sessions_ww, get_last_on_river_ww
+from datetime import datetime
+from db import load_data, get_total_sessions_ww_curr_year, get_last_on_river_ww
 
 st.set_page_config(
     page_title="AG Logs", layout="centered"
 )
+
+# load data 
+data_df = load_data()
 
 st.write("""
 # AG Logs  
@@ -14,12 +18,15 @@ See summaries below.
 """)  
 "---"
 
-multi = '''**WW Sessions**  
-Whitewater Log  
+multi = f'''Whitewater Log  
+**{datetime.now().year} WW Sessions**
 '''
 
 st.markdown(multi)
-st.markdown(f"### {get_total_sessions_ww()} sessions")
+st.markdown(f"### {get_total_sessions_ww_curr_year(data_df)} sessions" 
+            if get_total_sessions_ww_curr_year(data_df) > 1 
+            else f"### {get_total_sessions_ww_curr_year(data_df)} session")
+
 st.markdown(f"Last on the river on **{get_last_on_river_ww()}**")
 "---"
 
